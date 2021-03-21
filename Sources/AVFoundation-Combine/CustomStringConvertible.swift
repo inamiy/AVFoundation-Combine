adaddcdaec1a9f061c06359e0cdc953b73d10168
Yasuhiro Inami <inamiy@gmail.com>
2021-03-21 22:07:39 +0900

    Add `extension AVPlayerItem.Status: CustomStringConvertible`

--- Sources/AVFoundation-Combine/CustomStringConvertible.swift
+++ Sources/AVFoundation-Combine/CustomStringConvertible.swift
@@ -29,3 +29,18 @@ extension AVPlayer.TimeControlStatus: CustomStringConvertible {
         }
     }
 }
+
+extension AVPlayerItem.Status: CustomStringConvertible {
+    public var description: String {
+        switch self {
+        case .unknown:
+            return "unknown"
+        case .readyToPlay:
+            return "readyToPlay"
+        case .failed:
+            return "failed"
+        @unknown default:
+            return "@unknown"
+        }
+    }
+}
