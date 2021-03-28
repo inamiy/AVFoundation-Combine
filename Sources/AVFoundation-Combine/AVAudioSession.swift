5e16e2ebd409c2fe934855649c268e0675a69542
Yasuhiro Inami <inamiy@gmail.com>
2021-03-28 18:53:46 +0900

    Enable `AVAudioSession` extension only for macOS

--- Sources/AVFoundation-Combine/AVAudioSession.swift
+++ Sources/AVFoundation-Combine/AVAudioSession.swift
@@ -1,9 +1,11 @@
 import AVFoundation
 import Combine
 
+#if !os(macOS)
 extension AVAudioSession {
     public var outputVolumePublisher: AnyPublisher<Float, Never> {
         publisher(for: \.outputVolume)
             .eraseToAnyPublisher()
     }
 }
+#endif
